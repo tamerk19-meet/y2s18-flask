d9386eef0b615aa83f2f60662dbea16d326000f9
tamerk19-meet <tamerk19@meet.mit.edu>
2018-08-07 18:25:17 +0300

    I'm tired

--- exercises/app.py
+++ exercises/app.py
@@ -3,7 +3,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-    pass
+    x="I'd rather die"
+    return(x)
 
+
+@app.route('/fs')
+def favourite_sport():
+    return render_template("index.html")
+#################################
 if __name__ == '__main__':
    app.run(debug = True)
